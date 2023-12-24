b427c76eb4e44a235f228ca2fb2d1e8241d28279
Hawi-C <130748147+Hawi-C@users.noreply.github.com>
2023-12-25 00:01:35 +0100

    Update Shipping.jsx

--- src/components/cart/Shipping.jsx
+++ src/components/cart/Shipping.jsx
@@ -1,48 +1,62 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Country, State } from "country-state-city";
 import Popup from 'reactjs-popup';
 
 const Shipping = () => {
+  const [selectedCountry, setSelectedCountry] = useState('');
+  const [countries, setCountries] = useState([]);
+  const [states, setStates] = useState([]);
+
+  useEffect(() => {
+    setCountries(Country.getAllCountries());
+  }, []);
+
+  useEffect(() => {
+    if (selectedCountry) {
+      setStates(State.getStatesOfCountry(selectedCountry));
+    }
+  }, [selectedCountry]);
+
   return (
     <section className="shipping">
       <main>
         <h1>Shipping Details</h1>
         <form>
+          {/* Other form fields... */}
+          
           <div>
-            <label>H.No.</label>
-            <input type="text" placeholder="Enter House No." />
-          </div>
-          <div>
-            <label>City</label>
-            <input type="text" placeholder="Enter City" />
-          </div>
-          <div>
-              {/* Compelte the code for the COUNTRY DROPDOWN*/}
             <label>Country</label>
-
-            <select>
-              <option value="">Country</option>
-// Enter the code here for country dropdown           
-                  </option>
-                ))}
+            <select onChange={(e) => setSelectedCountry(e.target.value)}>
+              <option value="">Select Country</option>
+              {countries.map((country) => (
+                <option key={country.isoCode} value={country.isoCode}>
+                  {country.name}
+                </option>
+              ))}
             </select>
           </div>
+
           <div>
-              {/* Add the code for the STATE DROPDOWN*/}
-           
+            <label>State</label>
+            <select>
+              <option value="">Select State</option>
+              {states.map((state) => (
+                <option key={state.isoCode} value={state.isoCode}>
+                  {state.name}
+                </option>
+              ))}
+            </select>
           </div>
-          <div>
-            <label>Pin Code</label>
-            <input type="number" placeholder="Enter Pincode" />
-          </div>
-        // Enter thr code for contact           
+
+          {/* Other form fields... */}
           
-          <Popup trigger=
-                {<button type = "button">Confirm Order</button>}
-                position="right center">
-                <div style={{color:"red",position: 'absolute', top: '50%', right: '100%', transform: 'translateY(-50%)', backgroundColor: '#fff', padding: '10px', borderRadius: '5px', boxShadow: '0 0 10px rgba(0, 0, 0, 0.2)'}}>Order Placed</div>
-               
-            </Popup>
+          <Popup 
+            trigger={<button type="button">Confirm Order</button>} 
+            position="right center">
+            <div style={{color: "red", position: 'absolute', top: '50%', right: '100%', transform: 'translateY(-50%)', backgroundColor: '#fff', padding: '10px', borderRadius: '5px', boxShadow: '0 0 10px rgba(0, 0, 0, 0.2)'}}>
+              Order Placed
+            </div>
+          </Popup>
         </form>
       </main>
     </section>
