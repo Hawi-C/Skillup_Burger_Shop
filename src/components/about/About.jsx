1101090ff0712b014743f07e992b0ab65fbfe67a
Hawi-C <130748147+Hawi-C@users.noreply.github.com>
2023-12-25 00:00:22 +0100

    Update About.jsx

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,1 +1,43 @@
 // Write all the code here
+// Write all the code here
+import React from "react";
+import { Link } from "react-router-dom";
+import { RiFindReplaceLine } from "react-icons/ri";
+import me from "../../assets/skj.jpg";
+
+function About() {
+    return (
+        <div className="about-page">
+            <h1>About Us</h1>
+
+            <article>
+                <h4>Burger Shop</h4>
+                <p>
+                    This is Burger Shop. The place for the most tasty burgers on the
+                    entire earth.
+                </p>
+                <p>
+                    Explore the various types of food and burgers. Click below to see the
+                    menu.
+                </p>
+                <Link to="/menu"><RiFindReplaceLine /> View Menu</Link>
+            </article>
+
+            <div className="founder-section">
+                <h2>Founder</h2>
+                <article>
+                    <div className="founder-info">
+                        <img src={me} alt="Founder" />
+                        <h3>Nelson</h3>
+                    </div>
+                    <p>
+                        I am Nelson, the founder of Burger Shop. Affiliated to
+                        God Taste...
+                    </p>
+                </article>
+            </div>
+        </div>
+    );
+}
+
+export default About;
