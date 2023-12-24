aca7fac9b3b80ca374ff78a4ff2e9099f40ff808
Hawi-C <130748147+Hawi-C@users.noreply.github.com>
2023-12-25 00:03:36 +0100

    Update Home.jsx

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,4 +1,39 @@
 // Write all the code here
+// Write all the code here
+import React from "react";
+import { motion } from "framer-motion";
+import Founder from "./Founder";
+import Menu from "./Menu";
+
+function Home() {
+    const containerVariants = {
+        hidden: { opacity: 0, x: '100vw' },
+        visible: { opacity: 1, x: 0, transition: { type: 'spring', delay: 0.5 } }
+    };
+
+    return (
+        <motion.div 
+            className="home-container"
+            variants={containerVariants}
+            initial="hidden"
+            animate="visible"
+        >
+            <section className="founder-section">
+                <Founder />
+            </section>
+
+            <section className="menu-section">
+                <Menu />
+            </section>
+        </motion.div>
+    );
+}
+
+export default Home;
+
+
+
+
 
 
 
