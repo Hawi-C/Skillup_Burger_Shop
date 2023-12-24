dca5bdbf27bd8358ba135c3bd85fdfdf6bbe9f02
Hawi-C <130748147+Hawi-C@users.noreply.github.com>
2023-12-25 00:05:02 +0100

    Update Footer.jsx

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,1 +1,21 @@
 // Write all the code here
+// Write all the code here
+import React from 'react';
+import { AiFillYoutube, AiFillInstagram } from 'react-icons/ai';
+
+function Footer() {
+    return (
+        <footer>
+            <div className="social-media-bar">
+                <a href="https://www.youtube.com" target="_blank" rel="noopener noreferrer">
+                    <AiFillYoutube size={30} />
+                </a>
+                <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+                    <AiFillInstagram size={30} />
+                </a>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
