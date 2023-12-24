c9e8a6ebd6ad1712e8df3b9b9d7fac79652341e9
Hawi-C <130748147+Hawi-C@users.noreply.github.com>
2023-12-25 00:01:08 +0100

    Update Cart.jsx

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -1,53 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import burger1 from "../../assets/burger1.png";
 import burger2 from "../../assets/burger2.png";
-// import burger3 here
+import burger3 from "../../assets/burger3.png";
+import CartItem from "./CartItem"; 
 
-const CartItem = ({ value, title, img, increment, decrement }) => (
-  <div className="cartItem">
-    <div>
-      <h4>{title}</h4>
-      <img src={img} alt="Item" />
-    </div>
+function Cart() {
+  const [itemQuantities, setItemQuantities] = useState({ 1: 0, 2: 0, 3: 0 });
 
-    <div>
-      <button onClick={decrement}>-</button>
-      <input type="number" readOnly value={value} />
-      <button onClick={increment}>+</button>
-    </div>
-  </div>
-);
+  const increment = (itemId) => {
+    if (itemQuantities[itemId] < 10) {
+      setItemQuantities({
+        ...itemQuantities,
+        [itemId]: itemQuantities[itemId] + 1
+      });
+    }
+  };
 
-const Cart = () => {
-  const increment = (item) => {};
-
-  const decrement = (item) => {};
+  const decrement = (itemId) => {
+    if (itemQuantities[itemId] > 0) {
+      setItemQuantities({
+        ...itemQuantities,
+        [itemId]: itemQuantities[itemId] - 1
+      });
+    }
+  };
 
   return (
-    <section className="cart">
+    <section>
       <main>
         <CartItem
           title={"Cheese Burger"}
           img={burger1}
-          value={0}
+          value={itemQuantities[1]}
           increment={() => increment(1)}
-
-        // Add the function for decrementing the order by 1 
-       
+          decrement={() => decrement(1)}
         />
         <CartItem
           title={"Veg Cheese Burger"}
           img={burger2}
-          value={0}
+          value={itemQuantities[2]}
           increment={() => increment(2)}
-        // Add the function for decrementing the order by 2
-       
+          decrement={() => decrement(2)}
         />
-
-        {/* Fill up the code for Cheese Burger similarly */}
-       
-
+        <CartItem
+          title={"Cheese Burger with French Fries"}
+          img={burger3}
+          value={itemQuantities[3]}
+          increment={() => increment(3)}
+          decrement={() => decrement(3)}
+        />
         <article>
           <div>
             <h4>Sub Total</h4>
@@ -67,9 +69,14 @@ const Cart = () => {
           </div>
           <Link to="/shipping">Checkout</Link>
         </article>
+
+
+
+
+
       </main>
     </section>
   );
-};
+}
 
 export default Cart;
