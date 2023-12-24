364a48e6f3327e2e2bd7e7003766751ca88f34f0
Hawi-C <130748147+Hawi-C@users.noreply.github.com>
2023-12-25 00:04:17 +0100

    Update Menu.jsx

--- src/components/home/Menu.jsx
+++ src/components/home/Menu.jsx
@@ -1,1 +1,37 @@
 // Write all the code here
+// Write all the code here
+import React from "react";
+import MenuCard from "./MenuCard";
+import burger1 from "../../assets/burger1.png";
+import burger2 from "../../assets/burger2.png";
+import burger3 from "../../assets/burger3.png";
+
+function Menu() {
+    const menuItems = [
+        { id: 1, title: "Classic Burger", image: burger1, price: "$5.99" },
+        { id: 2, title: "Cheese Burger", image: burger2, price: "$6.99" },
+        { id: 3, title: "Veggie Burger", image: burger3, price: "$4.99" }
+    ];
+
+    const handleAddToCart = (itemId) => {
+        console.log("Added to cart:", itemId);
+    };
+
+    return (
+        <div className="menu">
+            {menuItems.map((item, index) => (
+                <MenuCard
+                    key={item.id}
+                    itemNumber={item.id}
+                    title={item.title}
+                    imageSrc={item.image}
+                    price={item.price}
+                    onAddToCart={() => handleAddToCart(item.id)}
+                    animationDelay={`${index * 0.2}s`} // Delay increases with each item
+                />
+            ))}
+        </div>
+    );
+}
+
+export default Menu;
